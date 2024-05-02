a5510d7262a25f35818ca57f6d82131992a19964
Raul23066 <140854151+Raul23066@users.noreply.github.com>
2024-05-02 08:46:10 -0500

    Update main.js

--- main.js
+++ main.js
@@ -543,7 +543,7 @@ setInterval(async () => {
   if (stopped === 'close' || !conn || !conn.user) return;
   const _uptime = process.uptime() * 1000;
   const uptime = clockString(_uptime);
-  const bio = `[ ⏳ ] Uptime: ${uptime} Monovan Bot`;
+  const bio = `[ ⏳ ] Uptime: ${uptime} DINO BOT`;
   await conn.updateProfileStatus(bio).catch((_) => _);
 }, 60000);
 function clockString(ms) {
